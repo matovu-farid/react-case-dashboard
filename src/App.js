c6579123f84b47c258b7cb5bf0c4e23199a90d77
matovu-farid <matovu90@gmail.com>
2022-03-05 18:38:09 +0300

    Configure App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import { Provider } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+// import Entry from './pages/Entry/Entry';
+import { fetchHospitals } from './redux/hospitals/hospitals';
+import Hospitals from './pages/Hospitals/Hospitals';
 import Entry from './pages/Entry/Entry';
-import store from './redux/configureStore';
+import Navbar from './Navbar/Navbar';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchHospitals());
+  }, []);
   return (
-    <Provider store={store}>
+    <>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Entry />} />
+        <Route path="/" element={<Hospitals />} />
+        <Route path="/entry/:id" element={<Entry />} />
+
       </Routes>
-    </Provider>
+    </>
   );
 }
 
