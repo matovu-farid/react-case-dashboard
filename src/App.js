c6234afbed5a4f7271879876ff2a4535a221bbde
matovu-farid <matovu90@gmail.com>
2022-03-05 23:54:04 +0300

    Add store

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-// import Entry from './pages/Entry/Entry';
 import { fetchHospitals } from './redux/hospitals/hospitals';
 import Hospitals from './pages/Hospitals/Hospitals';
 import Entry from './pages/Entry/Entry';
 import Navbar from './Navbar/Navbar';
+import Cities from './pages/Cities/Cities';
+import CityEntry from './pages/CityEntry/CityEntry';
+import { fetchCities } from './redux/cities/cities';
 
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchHospitals());
+  }, []);
+  useEffect(() => {
+    dispatch(fetchCities());
   }, []);
   return (
     <>
@@ -20,6 +25,8 @@ function App() {
         <Route path="/" element={<Hospitals />} />
         <Route path="/entry/:id" element={<Entry />} />
 
+        <Route path="/cities" element={<Cities />} />
+        <Route path="/cityentry/:id" element={<CityEntry />} />
       </Routes>
     </>
   );
